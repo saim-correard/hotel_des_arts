ebea2bfdf0a16026febbea863c1e183833eb51ee
Nonus59 <arnaudh59@gmail.com>
2017-09-16 17:57:40 +0200

    home-bstr

--- php/home.php
+++ php/home.php
@@ -2,7 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="../style.css"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
         <title>Hôtel</title>
     </head>
 
@@ -14,5 +16,7 @@
         </section>
 
         <?php include("footer.php"); ?>
+		
+		<?php include("js_bootstrap.php"); ?>
     </body>
 </html>
