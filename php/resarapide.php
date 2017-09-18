5ca4f791bea26af7d35b6f665e6ddd14d3323e53
Nonus59 <arnaudh59@gmail.com>
2017-09-18 19:49:52 +0200

    maj resa
    encore du taf, c'est une galère bootstrap...

--- php/resarapide.php
+++ php/resarapide.php
@@ -1,3 +1,43 @@
+<div class="container">
+  <section class="row justify-content-md-center">
+    <form class="form-inline" method="post" action="">
+
+      <select  class="form-control" name="" id="">
+        <option>Date d'arrivée</option>
+      </select>
+
+      <select class="form-control" name="" id="">
+        <option>Nombre de nuits</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+        <option>10</option>
+      </select>
+      <select class="form-control" name="" id="">
+        <option>Nombre de personnes</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+        <option>10</option>
+      </select>
+      <button type="submit" class="btn btn-primary">Rechercher</button>
+    </form>
+  </section>
+</div>
+
+<!-- OLD
 <section id="resarapide">
   <form method="post" action="">
     <select name="" id="">
@@ -32,3 +72,4 @@
     <input type="submit" />
   </form>
 </section>
+-->
