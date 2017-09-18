a134be5c7c242ac7148c93aff93065930459dcaa
Nonus59 <arnaudh59@gmail.com>
2017-09-18 19:13:55 +0200

    base de la page tarifs

---
+++ php/tarifs.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <title>Hôtel</title>
+    </head>
+
+    <body class="container">
+
+
+
+        <?php include("header.php"); ?>
+
+        <?php include("resarapide.php"); ?>
+
+        <h3>Nos chambres</h3>
+
+    		<table class="table">
+    		  <!--<thead>
+    			<tr>
+    			  <th><h4>Nos chambres</h4></th>
+    			  <th>Tarif / nuit</th>
+    			  <th>Username</th>
+    			</tr>
+        </thead>-->
+    		  <tbody>
+    			<tr>
+    			  <th scope="row">Chambre 1</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 2</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 3</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 4</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 5</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 6</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 7</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 8</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 9</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 10</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    			<tr>
+    			  <th scope="row">Chambre 11</th>
+    			  <td>140,00 €</td>
+    			  <td><a href="#">En savoir plus</a></td>
+    			</tr>
+    		  </tbody>
+    		</table>
+
+        <h3>Nos offres</h3>
+
+
+        <section class="row justify-content-between">
+          <div class="card col-lg4 col-md12 col-sm12" style="width: 20rem;">
+            <img class="card-img-top" src="../images/avantage.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+          <div class="card col-lg4 col-md12 col-sm12" style="width: 20rem;">
+            <img class="card-img-top" src="../images/avantage.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+
+          <div class="card col-lg4 col-md12 col-sm12" style="width: 20rem;">
+            <img class="card-img-top" src="../images/avantage.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </section>
+
+        <h3>Restauration</h3>
+
+          
+          <table class="table table-sm">
+      		  <!--<thead>
+      			<tr>
+      			  <th><h4>Nos chambres</h4></th>
+      			  <th>Tarif / nuit</th>
+      			  <th>Username</th>
+      			</tr>
+          </thead>-->
+      		  <tbody>
+      			<tr>
+      			  <th scope="row">plancha</th>
+      			  <td>14,00 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Café</th>
+      			  <td>1,50 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Bière</th>
+      			  <td>5,00 €</td>
+      			</tr>
+            <tr>
+      			  <th scope="row">plancha</th>
+      			  <td>14,00 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Café</th>
+      			  <td>1,50 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Bière</th>
+      			  <td>5,00 €</td>
+      			</tr>
+            <tr>
+      			  <th scope="row">plancha</th>
+      			  <td>14,00 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Café</th>
+      			  <td>1,50 €</td>
+      			</tr>
+      			<tr>
+      			  <th scope="row">Bière</th>
+      			  <td>5,00 €</td>
+      			</tr>
+      		  </tbody>
+      		</table>
+
+
+		    <?php include("js_bootstrap.php"); ?>
+
+        <?php include("footer.php"); ?>
+    </body>
+</html>
