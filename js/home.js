919ce1b7539a5b3d6f6c2ee53a3cca352891b1ce
Guillaume Crouzille <guillaumecrouzille@gmail.com>
2017-09-19 22:30:44 +0200

    affichage des news sur la home
    - affichage de la liste des news
    - déroulement de la news au clic

---
+++ js/home.js
@@ -0,0 +1,15 @@
+var news = document.querySelectorAll(".newsDesc");
+for (var e of news)
+{
+	e.addEventListener("click", function(e) {
+		var parent = e.currentTarget.parentNode;
+		if (parent.style.height == "500px") {
+			parent.style.height = "333px";
+			e.currentTarget.style.overflow = "hidden";
+		}
+		else {
+			parent.style.height = "500px";
+			e.currentTarget.style.overflow = "visible";
+		}
+	});
+}
--- php/home.php
+++ php/home.php
@@ -10,25 +10,52 @@
 
     <body class="container">   
 
-        <?php include("header.php"); ?>
+      <?php include("header.php"); ?>
 
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="../images/hotel_1.jpg" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="../images/hotel_2.jpg" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="../images/hotel_3.jpg" alt="Third slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="../images/hotel_4.jpg" alt="Fourth slide">
-            </div>
+
+      <section id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="../images/hotel_1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="../images/hotel_2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="../images/hotel_3.jpg" alt="Third slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="../images/hotel_4.jpg" alt="Fourth slide">
           </div>
         </div>
+      </section>
+
+      <section id="news">
+        <h2>News</h2>
+        <hr/>
+        <?php
+        include("bddConnexion.php");
+
+        $query = $bdd->query('SELECT * FROM news');
+
+        while($data = $query->fetch()) 
+        {
+        ?>
+        <!--On récupère toutes les news en bases pour les afficher-->
+          <div class="news">
+            <img src=<?php echo '"../images/news/'.htmlspecialchars($data['image']).'"'?> alt="image de l'hôtel"/>
+            <div class="newsDesc">
+              <h3><?php echo htmlspecialchars($data['titre'])?></h3>
+              <p><?php echo nl2br(htmlspecialchars($data['texte']))?></p>
+            </div>
+          </div>  
+        <?php
+        }
+        $query->closeCursor();
+      ?>
+      </section>
 		
 		<?php include("js_bootstrap.php"); ?>
     </body>
 </html>
+<script src="../js/home.js"></script>
