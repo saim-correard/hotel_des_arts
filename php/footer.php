c8c3b9e61e228e341a2962fccbf4b93d780c8c44
Nonus59 <arnaudh59@gmail.com>
2017-09-19 20:03:41 +0200

    amelioration page contact et footer
    Si on peut appeler ça une amélioration...

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,27 @@
 <footer>
-	<p> COPYRIGHT SUCE TOI 2017 </p>
+
+		<div class="row col-12">
+			<ul class="list-group col-4">
+			  <li class="list-group-item disabled">Nos Partenaires :</li>
+			  <li class="list-group-item">Dapibus ac facilisis in</li>
+			  <li class="list-group-item">Morbi leo risus</li>
+			  <li class="list-group-item">Porta ac consectetur ac</li>
+			  <li class="list-group-item">Vestibulum at eros</li>
+			</ul>
+			<ul class="list-group col-4">
+			  <li class="list-group-item disabled">Cras justo odio</li>
+			  <li class="list-group-item">Dapibus ac facilisis in</li>
+			  <li class="list-group-item">Morbi leo risus</li>
+			  <li class="list-group-item">Porta ac consectetur ac</li>
+			  <li class="list-group-item">Vestibulum at eros</li>
+			</ul>
+			<div class="card col-4" style="width: 20rem;">
+			  <img class="card-img-top" src="../images/avantage.jpg" alt="logo hôtel des Arts">
+			  <div class="card-body">
+			    <p class="card-text">Wimereux...</p>
+			  </div>
+			</div>
+		</div>
+
+	<p class="float-right"> Guillaume Crouzille / Arnaud Herbez </p>
 </footer>
