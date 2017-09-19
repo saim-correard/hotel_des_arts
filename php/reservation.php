83db3a3ef1d041612aa1952c2ee4ac59e48d9257
Nonus59 <arnaudh59@gmail.com>
2017-09-19 20:55:09 +0200

    page chemCde1 Reservation

---
+++ php/reservation.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../style.css"/>
+		    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <title>Hôtel</title>
+    </head>
+
+    <body class="container">
+
+      <?php include("header.php"); ?>
+
+      </br></br>
+
+      <?php include("resarapide.php"); ?>
+
+      </br></br>
+
+      <section class="row">
+          <img class="col col-lg-7 col-sm12" src="../images/chambres/chambre1_1.jpg" alt="Chambre 1" />
+          <aside class="col col-lg-5 col-sm12">
+              <h4>Chambre 1</h4>
+              <p>Description de la chambre lumineuse, située au premier étage. le colori pastel de cette chambre fait un bien à fou à Pochito. Arretez-le il devient fou !!</p>
+              <p>59,90 € / nuit</p>
+              <button type="button" class="btn btn-success btn-block">Je réserve</button>
+          </aside>
+      </section>
+
+      </br></br>
+
+      <section class="row">
+        <aside class="col col-lg-5 col-sm12">
+            <h4>Chambre 2</h4>
+            <p>Description de la chambre lumineuse, située au premier étage. le colori pastel de cette chambre fait un bien à fou à Pochito. Arretez-le il devient fou !!</p>
+            <p>59,90 € / nuit</p>
+            <button type="button" class="btn btn-success btn-block">Je réserve</button>
+        </aside>
+          <img class="col col-lg-7 col-sm12" src="../images/chambres/chambre1_2.jpg" alt="Chambre 2" />
+      </section>
+
+    </br></br>
+
+    <section class="row">
+        <img class="col col-lg-7 col-sm12" src="../images/chambres/chambre1_1.jpg" alt="Chambre 3" />
+        <aside class="col col-lg-5 col-sm12">
+            <h4>Chambre 3</h4>
+            <p>Description de la chambre lumineuse, située au premier étage. le colori pastel de cette chambre fait un bien à fou à Pochito. Arretez-le il devient fou !!</p>
+            <p>59,90 € / nuit</p>
+            <button type="button" class="btn btn-success btn-block">Je réserve</button>
+        </aside>
+    </section>
+
+    </br></br>
+
+    <section class="row">
+      <aside class="col col-lg-5 col-sm12">
+          <h4>Chambre 4</h4>
+          <p>Description de la chambre lumineuse, située au premier étage. le colori pastel de cette chambre fait un bien à fou à Pochito. Arretez-le il devient fou !!</p>
+          <p>59,90 € / nuit</p>
+          <button type="button" class="btn btn-success btn-block">Je réserve</button>
+      </aside>
+        <img class="col col-lg-7 col-sm12" src="../images/chambres/chambre1_2.jpg" alt="Chambre 4" />
+    </section>
+
+    </br></br>
+
+    <?php include("footer.php"); ?>
+
+    </body>
+</html>
