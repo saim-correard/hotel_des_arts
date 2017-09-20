3cbf24ff61a4cb379ba221501dac5bfb0441d478
Guillaume Crouzille <guillaumecrouzille@gmail.com>
2017-09-20 22:21:17 +0200

    retake page hotel
    intégration de bootstrap
    changement des articles en cards
    utilisation carousel bootstrap
    clean du css

--- php/hotel.php
+++ php/hotel.php
@@ -1,50 +1,74 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="../style.css"/>
-        <title>Hôtel des Arts</title>
-    </head>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../style.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">           
+    <title>Hôtel des Arts</title>
+  </head>
 
-    <body>   
-        <?php include("header.php"); ?>
+  <body class="container">   
+    <?php include("header.php"); ?>
+    <?php include("resarapide.php"); ?>
 
-        <?php include("resarapide.php"); ?>
-
-        <section id="hotel">
-            <section class="ligneArticleHotel">
-                <article class="articleHotel">
-                    <h2> Hôtel des Arts </h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </article>
-                <article class="articleHotel">
-                    <h2> La ville de Wimereux </h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </article>
-            </section>
-            <section class="ligneArticleHotel">
-                <article class="articleHotel">
-                    <h2> Nos chambres </h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </article>
-                <article class="articleHotel">
-                    <h2> Notre bar </h2>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </article>
-            </section>
-        </section>
-
-        <section>
-          <h3> Galerie Photos </h3>
-          <hr>
-          <div id="carousel">
-            <button id="left"><</button>
-            <img id="carousel_image" src="../images/hotel_1.jpg" alt="image de l'hôtel"/>
-            <button id="right">></button>
+    <section class="container">
+      <div class="row justify-content-between ligneArticleHotel">
+        <div class="card col-lg-5">
+          <img class="card-img-top" src="../images/card.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Hôtel des arts</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
-        </section>
+        </div>
+        <div class="card col-lg-5">
+          <img class="card-img-top" src="../images/card.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">La ville de Wimereux</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>              
+      </div>
+      <div class="row justify-content-between ligneArticleHotel">
+        <div class="card col-lg-5">
+          <img class="card-img-top" src="../images/card.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Nos Chambres</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div class="card col-lg-5">
+          <img class="card-img-top" src="../images/card.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Notre bar</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </section>
 
-        <script src="../js/hotel.js"></script>
-        
-    </body>
+    <section id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">            
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="../images/hotel_1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="../images/hotel_2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="../images/hotel_1.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="../images/hotel_2.jpg" alt="Fourth slide">
+        </div>
+      </div>
+    </section> 
+
+    <?php include("js_bootstrap.php"); ?>
+  </body>
 </html>
+
+
